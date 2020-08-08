5938c2e2e601f327af211a40eee210c1191b1c7b
PassionCourse <bilguudei.b8@gmail.com>
2020-08-09 02:59:43 +0800

    ub slider added, more responsive but text deleted

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-$(window).scroll(function () {
-    $('.animation-test').each(function () {
-        var imagePos = $(this).offset().top;
-        var imageHeight = $(this).height();
-        var topOfWindow = $(window).scrollTop();
+function submitHandler() {
+    alert("You have done the submission");
+}
 
-        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
-            $(this).addClass("slideRight");
-        } else {
-            $(this).removeClass("slideRight");
-        }
-    });
-});
+// document.getElementById("submitid").onclick = submitHundler
